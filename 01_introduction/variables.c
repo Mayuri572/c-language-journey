8d6c1ebed0d60239902bc68f53bf77bea94add8d
Mayuri <jmayuri572@gmail.com>
2026-01-02 14:09:44 +0530

    Updated Hello World program and added variables example

---
+++ 01_introduction/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// This program demonstrates the use of variables in C
+int main() {
+    int age = 20;
+    float height = 5.4;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
+
+// Output:
+// Age: 20  
+// Height: 5.4
+// Grade: A
