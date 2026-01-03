ccfe05fbe9fd83e781f87e9231eec88b8a384549
Mayuri <jmayuri572@gmail.com>
2026-01-03 14:47:34 +0530

    Added while loop example

---
+++ 04_loop_control/while_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Demonstration of while loop
+
+int main() {
+    int i = 1;
+
+    while (i <= 5) {
+        printf("Count: %d\n", i);
+        i++;
+    }
+
+    return 0;
+}
