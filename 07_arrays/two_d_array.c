cf5e13ccb85c01928872068098d72e8e73048bdf
Mayuri <jmayuri572@gmail.com>
2026-01-11 00:25:42 +0530

    Added 2D array example

---
+++ 07_arrays/two_d_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// 2D array example
+
+int main() {
+    int matrix[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//OUTPUT:
+//1 2 3
+//4 5 6
